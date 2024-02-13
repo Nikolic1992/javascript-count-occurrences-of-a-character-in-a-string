a75b3583b73ebb240cf70f05a0cdb870346d77b2
Stevan Nikolic <stevannikolic1992@yahoo.com>
2024-02-13 20:34:17 +0100

    Added javascript function that counts occurrences of a character in a string

---
+++ script.js
@@ -0,0 +1,7 @@
+function strCount(str, letter) {
+  return str.split(letter).length - 1;
+}
+
+console.log(strCount("Hello", "o")); // returns 1
+console.log(strCount("Hello", "l")); // returns 2
+console.log(strCount("", "z")); // returns 0
